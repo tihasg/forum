363904155d368a25a744f3e7066585fc04131344
Tiago Rodrigues <tiago.dev.lr@gmail.com>
2024-03-04 13:42:24 -0300

    iniciando get listagem topicos

--- src/main/kotlin/br/tihasg/com/foru/controller/HelloController.kt
+++ src/main/kotlin/br/tihasg/com/foru/controller/HelloController.kt
@@ -1,4 +1,4 @@
-package br.tihasg.com.foru
+package br.tihasg.com.foru.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
