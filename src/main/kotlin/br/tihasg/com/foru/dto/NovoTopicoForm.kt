a56006ccc3b2a503b585071d8fc43c52532d7142
Tiago Rodrigues <tiago.dev.lr@gmail.com>
2024-03-05 12:49:27 -0300

    tratamento de mensagem

--- src/main/kotlin/br/tihasg/com/foru/dto/NovoTopicoForm.kt
+++ src/main/kotlin/br/tihasg/com/foru/dto/NovoTopicoForm.kt
@@ -6,8 +6,8 @@ import jakarta.validation.constraints.Size
 
 data class NovoTopicoForm(
         @field:NotNull val id: Long,
-        @field:NotEmpty @field:Size(min = 5, max = 10) val title: String,
-        @field:NotEmpty val msg: String,
+        @field:NotEmpty @field:Size(min = 5, max = 10, message = "Titulo deve ter entre 5 a 100 caracteres") val title: String,
+        @field:NotEmpty(message = "nao pode ser vazio") val msg: String,
         @field:NotNull val autor: Long,
         @field:NotNull val curso: Long
 )
